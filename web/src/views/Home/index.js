eb8439c1a1a39c2e5c6f60b636f81910c7ef3a1b
Simon CHEN <csbdess@gmail.com>
2024-05-20 17:09:07 +0800

    fix: 着色部分放在box里面

--- web/src/views/Home/index.js
+++ web/src/views/Home/index.js
@@ -61,20 +61,20 @@ const Home = () => {
         <BaseIndex />
       ) : (
         <>
-          <Box>
+          <Box
+                              sx={{
+                                minHeight: 'calc(100vh - 136px)',
+                                backgroundImage: 'linear-gradient(to right, #ff9966, #ff5e62)',
+                                color: 'white',
+                                p: 4
+                              }}
+          >
             
             {homePageContent.startsWith('https://') ? (
               <iframe title="home_page_content" src={homePageContent} style={{ width: '100%', height: '100vh', border: 'none' }} />
             ) : (
               <>
-                <Container maxWidth="lg"
-                    sx={{
-                      minHeight: 'calc(100vh - 136px)',
-                      backgroundImage: 'linear-gradient(to right, #ff9966, #ff5e62)',
-                      color: 'white',
-                      p: 4
-                    }}
-                >
+                <Container maxWidth="lg">
                 <Grid container columns={12} wrap="nowrap" alignItems="center" sx={{ minHeight: 'calc(100vh - 230px)' }}>
                   <Grid md={7} lg={6}>
                   <Stack spacing={3}>
